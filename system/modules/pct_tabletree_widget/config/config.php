23c1df802685ba58a5730c9584c6ee5cae8bc7b1
Tim Gatzky <info@tim-gatzky.de>
2015-10-15 11:30:02 +0200

    update to version 1.3.2

--- system/modules/pct_tabletree_widget/config/config.php
+++ system/modules/pct_tabletree_widget/config/config.php
@@ -16,7 +16,7 @@
  * Constants
  */
 define(PCT_TABLETREE_PATH, 'system/modules/pct_tabletree_widget');
-define(PCT_TABLETREE_VERSION, '1.3.1');
+define(PCT_TABLETREE_VERSION, '1.3.2');
 
 /**
  * Back end form fields

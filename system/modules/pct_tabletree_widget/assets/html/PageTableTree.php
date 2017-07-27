37ae14f6343b85574e76605ea398c048b8b162b5
Tim Gatzky <info@tim-gatzky.de>
2017-07-27 17:22:46 +0200

    #12 locate subfolders

--- system/modules/pct_tabletree_widget/assets/html/PageTableTree.php
+++ system/modules/pct_tabletree_widget/assets/html/PageTableTree.php
@@ -16,10 +16,25 @@
 define('TL_MODE', 'BE');
 define('TL_SCRIPT', 'PCT_TABLETREE_WIDGET');
 
-// contao 3 structure
-if( file_exists( realpath($_SERVER['DOCUMENT_ROOT']) . '/system/initialize.php') )
+$_subfolder = '';
+$_subs = array();
+$_dirs = array_filter(explode('/', $_SERVER['SCRIPT_NAME']));
+foreach($_dirs as $i => $part)
 {
-	require_once realpath($_SERVER['DOCUMENT_ROOT']) . '/system/initialize.php';
+	if($part == 'system' && $_dirs[$i+1] == 'modules')
+	{
+		break;
+	}
+	$_subs[] = $part;
+}
+if(count($_subs) > 0)
+{
+	$_subfolder = '/'.implode('/', $_subs).'/';
+}
+
+if( file_exists( realpath($_SERVER['DOCUMENT_ROOT']). $_subfolder . '/system/initialize.php') )
+{
+	require_once realpath($_SERVER['DOCUMENT_ROOT']). $_subfolder . '/system/initialize.php';
 }
 // contao 4 structure runs in a relative subfolder
 else if( file_exists( realpath($_SERVER['DOCUMENT_ROOT'].'/../') . '/system/initialize.php') )
@@ -30,6 +45,10 @@ else
 {
 	throw new \Exception('Contaos initialize.php not found in: '.$path_to_initialize);
 }
+
+unset($_subs);
+unset($_dirs);
+unset($_subfolder);
 
 /**
  * Instantiate the controller

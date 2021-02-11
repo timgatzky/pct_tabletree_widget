b2899412036c596d3d6a19df449ea93e892d123d
Tim Gatzky <info@tim-gatzky.de>
2021-02-11 09:45:21 +0100

    update to version 1.6.5

--- system/modules/pct_tabletree_widget/config/config.php
+++ system/modules/pct_tabletree_widget/config/config.php
@@ -16,7 +16,7 @@
  * Constants
  */
 define('PCT_TABLETREE_PATH', 'system/modules/pct_tabletree_widget');
-define('PCT_TABLETREE_VERSION', '1.6.4');
+define('PCT_TABLETREE_VERSION', '1.6.5');
 
 /**
  * Back end form fields

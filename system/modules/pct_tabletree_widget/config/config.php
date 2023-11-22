2dc67f4f6ed3a4e4c2f62ff3f9178a0f3770d591
Tim Gatzky <info@tim-gatzky.de>
2023-11-22 08:15:55 +0100

    update to 1.8.0

--- system/modules/pct_tabletree_widget/config/config.php
+++ system/modules/pct_tabletree_widget/config/config.php
@@ -16,7 +16,7 @@
  * Constants
  */
 define('PCT_TABLETREE_PATH', 'system/modules/pct_tabletree_widget');
-define('PCT_TABLETREE_VERSION', '1.7.4');
+define('PCT_TABLETREE_VERSION', '1.8.0');
 
 /**
  * Back end form fields
